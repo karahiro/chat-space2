9e60908d8045f0f3428d489c0a284d0281464cdc
karahiro <taro6340@gmail.com>
2018-04-26 04:25:35 +0900

    自動更新機能の追加
    #what
    自動更新機能を追加しました
    #why
    必要な機能だから

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,16 +2,15 @@ $(document).on("turbolinks:load", function() {
 
     $(function(){
     setInterval(update, 5000);
-    //5000ミリ秒ごとにupdateという関数を実行する
   });
 
   function flash() {
     var html =`<p class="notice">メッセージを送信しました</p>`
     $('.notification').append(html);
-    $('.notice').fadeIn(500).fadeOut(2000); //指定したクラスを0.5秒でfade inさせて、2秒でfade outさせる。
+    $('.notice').fadeIn(500).fadeOut(2000);
     setTimeout(function(){
      $('.notice').remove();
-    },2500); //指定のクラス自体をremoveする。
+    },2500);
   }
 
   function buildHTML(message){
@@ -64,24 +63,24 @@ $(document).on("turbolinks:load", function() {
   });
 
 
-  function update(){ //この関数では以下のことを行う
-     if($('.chat-main__body--id')[0]){ //もし'messages'というクラスがあったら
-      var message_id = $('.chat-main__body--id:last').data('messageId'); //一番最後にある'messages'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
-    } else { //ない場合は
-      var message_id = 0 //0を代入
+  function update(){
+     if($('.chat-main__body--id')[0]){
+      var message_id = $('.chat-main__body--id:last').data('messageId');
+    } else {
+      var message_id = 0
     }
 
-    var message_id = $('.chat-main__body--id:last').data('messageId'); //一番最後にある'messages'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
-   $.ajax({ //ajax通信で以下のことを行う
-      url: location.href, //urlは現在のページを指定
-      type: 'GET', //メソッドを指定
-      data: { //railsに引き渡すデータは
-        message: { id: message_id } //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
+    var message_id = $('.chat-main__body--id:last').data('messageId');
+   $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {
+        message: { id: message_id }
       },
-      dataType: 'json' //データはjson形式
+      dataType: 'json'
     })
     .done(function(data){
-      $.each(data, function(i, data){ //'data'を'data'に代入してeachで回す
+      $.each(data, function(i, data){
         var html = buildHTML(data);
         $('.chat-main__body').append(html)
         var position = $('#chat-main__body').offset().top;
@@ -90,11 +89,3 @@ $(document).on("turbolinks:load", function() {
     });
   }
 });
-
-// .done(function(data){
-//  +      if (data.length == 0) return false
-//  +      data.forEach(function(msg) {
-//  +        var html = buildHTML(msg)
-//  +        $('.chat-main__body--messages-list').append(html)
-//  +      })
-
